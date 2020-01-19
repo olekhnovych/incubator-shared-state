b4fb2ba765763e08ddb2a04531fd0bf9db668e36
Sergii Olekhnovych <sergii@thinkpad.emea.local>
2020-01-19 14:54:05 +0100

    propagate status example

---
+++ src/main/scala/com/synerise/ai/sharedstate/example/propagatestatus.scala
@@ -0,0 +1,106 @@
+package com.synerise.ai.sharedstate.examples.propagatestatus
+
+import akka.actor.typed.scaladsl.{Behaviors, LoggerOps}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+
+import com.synerise.ai.sharedstate._
+import com.synerise.ai.sharedstate.condition._
+
+
+object SharedStateFactory {
+  def serviceStatus(owner: String, status: String) =
+    SharedState(Map("type"->"serviceStatus",
+                    "owner"->owner,
+                    "status"->status),
+                Set("owner", "type"))
+}
+
+
+object Service {
+  trait Message
+  case class SharedStatesResponce(sharedStates: SharedStates) extends Message
+  case class Print() extends Message
+  case class UpdateStatus(status: String) extends Message
+
+  def apply(storage: Storage.Ref, serviceName: String, watchedServiceNames: List[String]) = new Service(storage, serviceName, watchedServiceNames)()
+}
+
+class Service(val storage: Storage.Ref, serviceName: String, watchedServiceNames: List[String]) {
+  def apply(): Behavior[Service.Message] = Behaviors.setup { context =>
+
+    val sharedStatesWrapper: ActorRef[SharedStates] =
+      context.messageAdapter(sharedStates => Service.SharedStatesResponce(sharedStates))
+
+
+    val a = And(FieldEquals("type", "serviceStatus"),
+                Or(watchedServiceNames.map(watchedServiceName => FieldEquals("serviceName", watchedServiceName)): _*))
+    context.log.info(s"condition: $a")
+
+
+    storage ! Storage.Subscribe(And(FieldEquals("type", "serviceStatus"),
+                                    Or(watchedServiceNames.map(watchedServiceName => FieldEquals("serviceName", watchedServiceName)): _*)),
+                                sharedStatesWrapper)
+
+    lazy val exposeStatus: (String, String) => Unit = (status, watchedStatuses) =>
+      storage ! Storage.Update(SharedStateFactory.serviceStatus(serviceName, f"${status} (${watchedStatuses})"))
+
+    lazy val loop: (String, String) => Behavior[Service.Message] =
+      (status, watchedStatuses) => Behaviors.receiveMessage {
+        case Service.SharedStatesResponce(sharedStates) => {
+          val watchedStatuses = sharedStates.map(x => s"${x.fields("serviceName")}: ${x.fields("status")}").mkString(", ")
+
+          exposeStatus(status, watchedStatuses)
+          loop(status, watchedStatuses)
+        }
+        case Service.Print() => {
+          context.log.info(s"status: $status, watchedStatuses: $watchedStatuses")
+          Behaviors.same
+        }
+        case Service.UpdateStatus(newStatus) => {
+          exposeStatus(newStatus, watchedStatuses)
+          loop(newStatus, watchedStatuses)
+        }
+      }
+
+    exposeStatus("NOT READY", "")
+    loop("NOT READY", "")
+  }
+}
+
+
+object Run extends App {
+  implicit val system: ActorSystem[Spawner.Message] =
+    ActorSystem(Spawner(), "main")
+
+  val storage = Spawner.spawn(Storage(), "storage")
+  val serviceA = Spawner.spawn(Service(storage, "serviceA", List("serviceB", "serviceC")), "serviceA")
+  val serviceB = Spawner.spawn(Service(storage, "serviceB", List.empty), "serviceB")
+  val serviceC = Spawner.spawn(Service(storage, "serviceC", List.empty), "serviceC")
+
+  def print() = {
+    Thread.sleep(20)
+    storage ! Storage.Print()
+    Thread.sleep(20)
+    serviceA ! Service.Print()
+    Thread.sleep(20)
+    serviceB ! Service.Print()
+    Thread.sleep(20)
+    serviceC ! Service.Print()
+  }
+
+  print()
+
+  // storage ! Storage.Update(SharedStateFactory.requiredEnabledService("main", "serviceA", true))
+  // print()
+
+  // storage ! Storage.Update(SharedStateFactory.requiredEnabledService("main", "serviceB", true))
+  // print()
+
+  // storage ! Storage.Update(SharedStateFactory.requiredEnabledService("main", "serviceA", false))
+  // print()
+
+  // storage ! Storage.Update(SharedStateFactory.requiredEnabledService("main", "serviceB", false))
+  // print()
+
+
+}
