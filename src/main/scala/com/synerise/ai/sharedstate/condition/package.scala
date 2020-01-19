aec1a4478137e9402b84a778d68553e095286602
Sergii Olekhnovych <sergii@thinkpad.emea.local>
2020-01-19 14:53:40 +0100

    print conditions

--- src/main/scala/com/synerise/ai/sharedstate/condition/package.scala
+++ src/main/scala/com/synerise/ai/sharedstate/condition/package.scala
@@ -4,20 +4,28 @@ package com.synerise.ai.sharedstate
 package object condition {
   case class True() extends Condition {
     def apply(sharedState: SharedState): Boolean = true
+
+    override def toString: String = "true"
   }
 
   case class And(conditions: Condition*) extends Condition {
     def apply(sharedState: SharedState): Boolean =
       conditions.map(_(sharedState)).foldLeft(true)(_ && _)
+
+    override def toString: String = s"(${conditions.mkString(" AND ")})"
   }
 
   case class Or(conditions: Condition*) extends Condition {
     def apply(sharedState: SharedState): Boolean =
       conditions.map(_(sharedState)).foldLeft(false)(_ || _)
+
+    override def toString: String = s"(${conditions.mkString(" OR ")})"
   }
 
   case class FieldEquals(fieldName: String, value: String) extends Condition {
     def apply(sharedState: SharedState): Boolean =
       sharedState.fields.get(fieldName).map(_ == value).getOrElse(false)
+
+    override def toString: String = s"(${fieldName} == '${value})'"
   }
 }
