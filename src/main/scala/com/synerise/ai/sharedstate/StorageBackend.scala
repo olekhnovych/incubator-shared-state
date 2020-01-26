8a96bb64b9cf868a57acb58c0693a586e12952e0
Sergii Olekhnovych <sergii.olekhnovych@gmail.com>
2020-01-26 21:09:32 +0100

    typo

--- src/main/scala/com/synerise/ai/sharedstate/StorageBackend.scala
+++ src/main/scala/com/synerise/ai/sharedstate/StorageBackend.scala
@@ -1,7 +1,7 @@
 package com.synerise.ai.sharedstate
 
 trait StorageBackend {
-  case class UpdateResult(backend: StorageBackend, accepted: Boolean)
+  case class UpdateResult(storageBackend: StorageBackend, accepted: Boolean)
 
   def update(sharedState: SharedState, updateVersion: Boolean): UpdateResult
   def fetch(condition: Condition): SharedStates
